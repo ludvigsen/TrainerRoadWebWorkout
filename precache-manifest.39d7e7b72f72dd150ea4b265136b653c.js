f4c0a8adf5e1e793a952a8c39dc3ecaaeb820b40
Marius Skaar Ludvigsen <malu@nextgentel.com>
2020-02-06 13:46:59 +0100

    Updates

---
+++ precache-manifest.39d7e7b72f72dd150ea4b265136b653c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fbdcbe358122c062917239a85a3c40d",
+    "url": "/TrainerRoadWebWorkout/index.html"
+  },
+  {
+    "revision": "e0d67f63160c3c239157",
+    "url": "/TrainerRoadWebWorkout/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d1666935ac424d5d398e",
+    "url": "/TrainerRoadWebWorkout/static/js/2.db4bb9f1.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/TrainerRoadWebWorkout/static/js/2.db4bb9f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0d67f63160c3c239157",
+    "url": "/TrainerRoadWebWorkout/static/js/main.9db333d8.chunk.js"
+  },
+  {
+    "revision": "93bdf825f125b6b58dad",
+    "url": "/TrainerRoadWebWorkout/static/js/runtime-main.d42fdba3.js"
+  }
+]);
